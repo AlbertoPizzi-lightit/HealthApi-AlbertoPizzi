cb602773d8b53d710c663ecac9b5f5e32fcc0736
Alberto Pizzi <albertopizzi@Albertos-MacBook-Pro.local>
2025-11-03 14:07:18 -0300

    fix: remove RefreshAction

--- src/Authentication/App/Controllers/RefreshController.php
+++ src/Authentication/App/Controllers/RefreshController.php
@@ -7,18 +7,25 @@ namespace Lightit\Authentication\App\Controllers;
 use Dedoc\Scramble\Attributes\Group;
 use Illuminate\Http\JsonResponse;
 use Lightit\Authentication\App\Resources\RefreshResource;
-use Lightit\Authentication\Domain\Actions\RefreshAction;
+use Lightit\Authentication\Domain\DataTransferObjects\RefreshRequestDto;
 use PHPOpenSourceSaver\JWTAuth\Factory as JWTAuth;
 use PHPOpenSourceSaver\JWTAuth\JWT;
 
 #[Group('auth')]
 class RefreshController
 {
-    public function __invoke(JWTAuth $jwtAuth, JWT $jwt, RefreshAction $action): JsonResponse
+    public function __invoke(JWTAuth $jwtAuth, JWT $jwt): JsonResponse
     {
-        $refreshDto = $action->execute($jwtAuth, $jwt);
+       $refreshRequestDto = $this->toDto($jwtAuth , $jwt);
 
-        return RefreshResource::make($refreshDto)
+        return RefreshResource::make($refreshRequestDto)
             ->response();
+    }
+
+    public function toDto(JWTAuth $jwtAuth, JWT $jwt) : RefreshRequestDto{
+        return new RefreshRequestDto(
+            $jwtAuth,
+            $jwt
+        );
     }
 }
